d09689c0417c875e8c2f507ff05c90e35b7e8355
geena1102 <159452808+geena1102@users.noreply.github.com>
2024-05-13 12:03:58 +0530

    Add files via upload
    Its a python project for password generator. The user can give the number of letters, numbers and special characters as our needs .According to the number password is generated randomly in a simple and hard level

---
+++ project2_password_generator_hardlevel.py
@@ -0,0 +1,33 @@
+import random
+letters=['a','b','c','d','e','f','g','h','i','j','k',
+         'l','m','n','o','p','q','r','s','t','u','v'
+         'w','x','y','z','A','B','C','D','E','F','G',
+         'H','I','J','K','L','M','N','O','P','Q','R',
+         'S','T','U','V','W','X','Y','Z']
+numbers=['0','1','2','3','4','5','6','7','8','9']
+symbols=['!','#','$','%','&','(',')','*','+']
+print("Welcome to password generator")
+n_letters=int(input("How many letters you want in your password ?\n"))
+n_numbers=int(input("How many numbers you want in your password ?\n"))
+n_symbols=int(input("How many symbols you want in your password ?\n"))
+password_list=[]
+for i in range(1,n_letters+1) :
+    char=random.choice(letters)
+    #password_list=password_list+char
+    password_list +=char
+for j in range(1,n_numbers+1) :
+    num=random.choice(numbers)
+    password_list += num
+    #password_list=password_list+num
+for k in range(1,n_symbols+1) :
+    sim=random.choice(symbols)
+    password_list += sim
+    #password_list=password_list+sim
+random.shuffle(password_list)
+password=""
+for i in password_list :
+    password +=i
+print(f"Password is {password}")
+
+
+
